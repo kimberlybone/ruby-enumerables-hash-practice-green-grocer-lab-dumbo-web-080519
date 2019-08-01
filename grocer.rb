7e197d0c62aede2a2cad659e46c18613d6e79064
Kimberly Bone <k_bone@uncg.edu>
2019-08-01 17:04:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,7 +5,7 @@
   cart.each do |element|
     element.each do |food, hash|
       if new_cart[food]
-        new_cart[name][:count]
+        new_cart[name][:count] += 1
       # new_cart[food] ||= hash 
       # new_cart[food][:count] ||= 0 
       # new_cart[food][:count] += 1  
