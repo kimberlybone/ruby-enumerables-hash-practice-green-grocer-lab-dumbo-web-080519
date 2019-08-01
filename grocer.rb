89ec31fd8d39f1d98e0e35d684cd6fe3ab3fd28c
Kimberly Bone <k_bone@uncg.edu>
2019-08-01 17:22:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,7 +48,7 @@ end
 def checkout(cart, coupons)
   total = 0
   new_cart = consolidate_cart(cart)
-  coupon_cart = apply_coupons(consolidated, coupons)
+  coupon_cart = apply_coupons(new_cart, coupons)
   clearance_cart = apply_clearance(coupon_cart)
   clearance_cart.each do |item, attribute|
     total += (attribute[:price] * attribute[:count])
